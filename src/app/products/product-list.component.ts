6231504b404ac0bfd63fdeded89e84fbde819592
Nitisha Yadav <nitishayadav102@gmail.com>
2021-11-01 12:11:03 +0530

    ndings`

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,10 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
 })
 export class ProductListComponent{
 pageTitle:string = 'Product List';
+imageWidth = 50;
+imageMargin = 2;
+showImage : boolean = false;
+listFilter: string = "cart";
 products: any[] = [{
     "productId":2,
     "productName": "Garden Cart",
@@ -14,7 +18,11 @@ products: any[] = [{
     "description": "15 gallon capacity",
     "price": 32.29,
     "starRating":4.2,
-    "imageUrl":""
+    "imageUrl":"assets/images/hammer.png"
 
 }];
+
+toggleImage() : void{
+    this.showImage = !this.showImage;
+}
 }
